1abe7da06b6beeacc5a342036f33459986e3dc32
Ярослав Нестеров <atomofiron@yandex.ru>
2017-06-06 15:31:09 +0300

    back button actions edit

--- app/src/main/java/ru/atomofiron/boomstream/activities/MainActivity.kt
+++ app/src/main/java/ru/atomofiron/boomstream/activities/MainActivity.kt
@@ -87,14 +87,12 @@ class MainActivity : AppCompatActivity(), NotesAdapter.OnMediaClickListener, Nav
                 openedMedia = null
                 return
             }
-
-            super.onBackPressed()
-            startActivity(Intent(this, ApikeyActivity::class.java))
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+            R.id.nav_apikey -> startActivity(Intent(this, ApikeyActivity::class.java))
             R.id.nav_settings -> startActivity(Intent(this, SettingsActivity::class.java))
             R.id.nav_exit -> finish()
         }
